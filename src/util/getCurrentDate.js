d1f1127c768edffa0acaca248ff9bd017585ba42
Igboanugwo Collins <munisco12@gmail.com>
2021-01-16 10:02:52 +0100

    working on the header

--- src/util/getCurrentDate.js
+++ src/util/getCurrentDate.js
@@ -3,7 +3,9 @@ import React from 'react'
 const getCurrentDate = () => {
     let newDate = new Date()
 
-    const out = newDate.toLocaleString('en-GB', { minute: 'numeric', month: 'long', weekday: 'long', hour: 'numeric', hour12: true })
+    const out = newDate.getTime()
+
+   // const out = newDate.toLocaleString('en-GB', { minute: 'numeric', month: 'long', weekday: 'long', hour: 'numeric', hour12: true })
 
 
     return <p>{out}</p>
