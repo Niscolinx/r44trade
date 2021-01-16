38c6b9ff3e5b8ef8bf09c33073b21622381408cd
Igboanugwo Collins <munisco12@gmail.com>
2021-01-16 09:26:02 +0100

    Added the icons

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import { SiEthereum, SiLitecoin } from 'react-icons/si'
 import {RiBitCoinLine} from 'react-icons/ri'
 import { GiVikingLonghouse } from 'react-icons/gi'
