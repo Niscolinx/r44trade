87bf60d030bfcf7918e0922c0d4578f2c19669e4
Igboanugwo Collins <munisco12@gmail.com>
2021-01-16 08:49:07 +0100

    Added the icons

--- src/containers/AboutUs.js
+++ src/containers/AboutUs.js
@@ -27,170 +27,111 @@ display: inline-block;
 font-size: 16px;} */}
 
 
-<div style="position: fixed; z-index: 99999; left: 20px; bottom: 20px;">
-	<a href="http://wa.me/+447723536106">
-	<img src="https://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png" width="50px" />
-</a>
-</div>
+    <div style="position: fixed; z-index: 99999; left: 20px; bottom: 20px;">
+        <a href="http://wa.me/+447723536106">
+        <img src="https://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png" width="50px" />
+    </a>
+    </div>
 
-<div class="bodyWrap">
-<header>
-<div class="topLine">
-<div class="container">
-<div class="table full">
-<div class="logoWrap tCell">
-<div class="logo invisLink"> 
-<img src="img/logos.png" alt="logo" height="70px;" />
-Robot 22 Trade
-<a href="index.php">main</a>
-</div>
-</div>
-<div class="logReg tCell right">
-<a href="login.php" class="btnFillColor1Sm">login</a>
-<a href="register.php" class="btnFillColor2Sm">sign up</a>
-</div>
+    <div class="bodyWrap">
 
-<div class="socContHead lang tCell right">
-<a href="#" style="background-image: url(img/lang_1.jpg);"></a>
-<a href="#" style="background-image: url(img/lang_2.jpg);"></a>
-</div>
+    <div class="pageBanner" style="background-image: url(img/about_page_bg.jpg);">
+    <div class="container">
+    <span class="heading">About us</span>
+    </div>
+    </div>
 
-
-
-
-<div class="clock tCell right">
-<span class="time gradTxt" id="time"></span>
-<span class="date">
-<span class="day" id="day"></span>
-<span class="year" id="year"></span>
-</span>
-</div>
-
-
-
-
-</div>
-</div>
-</div>
-
-<script src="../widgets.coingecko.com/coingecko-coin-price-marquee-widget.js"></script>
-<coingecko-coin-price-marquee-widget  coin-ids="bitcoin,ethereum,eos,ripple,litecoin" currency="usd" background-color="#ffffff" locale="en"></coingecko-coin-price-marquee-widget>
-
-<div class="menu">
-<div class="container">
-<center><ul>
-<li><a href="index.php">HOME</a></li>
-<li><a href="about.php">ABOUT US</a></li>
-<li><a href="top-reward.php">TOP REWARD</a></li>
-<li><a href="pricing.php">PRICING</a></li>
-<li><a href="testimonies.php">Testimonies</a></li> 
-<li><a href="contact.php">CONTACT US</a></li>
-<li><a href="faq.php">FAQ</a></li>
-</ul></center>
-</div>
-</div>
-</header>
+    <div class="pageAbout" style="background-image: url(img/grey_bg_3.png)">
+    <div class="container">
+    <div class="textBlock">
+    <div class="info right">
+    <div class="license">
+    <div class="img invisLink" style="background-image: url(img/qr-code.jpg);"><a href="https://beta.companieshouse.gov.uk/company/12143668" target="_blank">license</a></div>
+    
+    </div>
+    <div class="address">
+    <div class="iconLeft">
+    <span class="icon-location gradTxt"></span>
+    <span class="data">71-75 Shelton Street, London, Greater London,
+    United Kingdom, WC2H 9JQ</span>
+    </div>
+    </div>
+    <div class="btnsWrap" style="text-align: center;">
+    <a href="register.php" class="btnFillColor1Lg" style="margin-right: 20px;">Join</a>
+    <a href="https://beta.companieshouse.gov.uk/company/12143668" target="_blank" class="btnFillColor2Lg" style="margin: 20px 0 0;">Check registration</a>
+    </div>
+    </div>
+    <p>
+    Robot 22 Trade is the only company dedicated to work on EA, better known as Algo Trading, which atomizes the trading process and brings risk free profit, saves times, money and build better wealth over long period of time.
+    </p>
+    <p>
+    A Robot trading System is used to automatically generate trading signals on the forex trader’s behalf. As such, a manual trader has to glue himself or herself in front of a computer screen and wait for the appropriate time when the criteria for making a decision fits with his or her rules—something which is tiring and complicated.
+    </p>
+    <p>
+    With robot trading, the trading rules or strategies have been ingrained in a computer program, and your trade decisions will be made automatically without your intervention. With a trading robot, you can trade profitably 24 hours a day and increase your profits. After you’ve coded your strategies on a robot, it will perform the buy and sell actions while you spend your time on the beach. One of the main reasons why traders fail is emotional trading. However, a forex robot lowers or eliminates emotions from the trading equation and give you a reliable way of earning money.
+    </p>
 
 
-
-
-<div class="pageBanner" style="background-image: url(img/about_page_bg.jpg);">
-<div class="container">
-<span class="heading">About us</span>
-</div>
-</div>
-
-<div class="pageAbout" style="background-image: url(img/grey_bg_3.png)">
-<div class="container">
-<div class="textBlock">
-<div class="info right">
-<div class="license">
-<div class="img invisLink" style="background-image: url(img/qr-code.jpg);"><a href="https://beta.companieshouse.gov.uk/company/12143668" target="_blank">license</a></div>
- 
-</div>
-<div class="address">
-<div class="iconLeft">
-<span class="icon-location gradTxt"></span>
-<span class="data">71-75 Shelton Street, London, Greater London,
-United Kingdom, WC2H 9JQ</span>
-</div>
-</div>
-<div class="btnsWrap" style="text-align: center;">
-<a href="register.php" class="btnFillColor1Lg" style="margin-right: 20px;">Join</a>
-<a href="https://beta.companieshouse.gov.uk/company/12143668" target="_blank" class="btnFillColor2Lg" style="margin: 20px 0 0;">Check registration</a>
-</div>
-</div>
-<p>
-Robot 22 Trade is the only company dedicated to work on EA, better known as Algo Trading, which atomizes the trading process and brings risk free profit, saves times, money and build better wealth over long period of time.
-</p>
-<p>
-A Robot trading System is used to automatically generate trading signals on the forex trader’s behalf. As such, a manual trader has to glue himself or herself in front of a computer screen and wait for the appropriate time when the criteria for making a decision fits with his or her rules—something which is tiring and complicated.
-</p>
-<p>
-With robot trading, the trading rules or strategies have been ingrained in a computer program, and your trade decisions will be made automatically without your intervention. With a trading robot, you can trade profitably 24 hours a day and increase your profits. After you’ve coded your strategies on a robot, it will perform the buy and sell actions while you spend your time on the beach. One of the main reasons why traders fail is emotional trading. However, a forex robot lowers or eliminates emotions from the trading equation and give you a reliable way of earning money.
-</p>
-
-
-<div class="img left" style="background-image: url(img/about_img.jpg);"></div>
-<div class="cfix"></div>
-<br/><br/>
-<p>
-Using a blockchain is cryptographically secure – the ledger is distributed across all nodes in the system making hacking near impossible. All of the solidity Ethereum smart contract code used for the token sale and vault account was fully audited by SmartDec, a firm specialized in Smart Contract Security Audit. No single authority has control.
-</p>
-</div>
-<div class="advTempl">
-<div class="container">
-<span class="captionDark center">Why choose us?</span>
-<div class="advWrap">
-<div class="grid">
-<div class="item col4">
-<div class="in">
-<span class="icon-phone gradTxt"></span>
-<span class="title">AUTOMATED</span>
-<p>No financial advisor? No experience trading cryptocurrency? No worries. Our algorithm takes away the guesswork and executes transactions automatically on your behalf.</p>
-</div>
-</div>
-<div class="item col4">
-<div class="in">
-<span class="icon-chart gradTxt"></span>
-<span class="title">SMART TRADING</span>
-<p>Emotions and stress have a negative impact on trading. Our Automated platform allows you to avoid that and gets the best option by analysing all data storage.</p>
-</div>
-</div>
-<div class="item col4">
-<div class="in">
-<span class="icon-team gradTxt"></span>
-<span class="title">PROTECTION FROM HACKING</span>
-<p>On a decentralized platform, all user accounts are independent; if one account is hacked, this won’t breach the security.</p>
-</div>
-</div>
-<div class="item col4">
-<div class="in">
-<span class="icon-docs gradTxt"></span>
-<span class="title">LOW COST</span>
-<p>Robot Trading only use money as cost of operation from profit, Thus RTD is ICO lowest cost of operations.</p>
-</div>
-</div>
-<div class="item col4">
-<div class="in">
-<span class="icon-cash gradTxt"></span>
-<span class="title">NO MORE MIDDLE MEN</span>
-<p>Immediate funding without third-parties. Smart contracts autonomously perform funding – collect and release payments.</p>
-</div>
-</div>
-<div class="item col4">
-<div class="in">
-<span class="icon-pass gradTxt"></span>
-<span class="title">AUTO TRADING 24/7</span>
-<p>Our platform is always available to ensure that no market opportunity is lost. Let our platform work for you!</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+    <div class="img left" style="background-image: url(img/about_img.jpg);"></div>
+    <div class="cfix"></div>
+    <br/><br/>
+    <p>
+    Using a blockchain is cryptographically secure – the ledger is distributed across all nodes in the system making hacking near impossible. All of the solidity Ethereum smart contract code used for the token sale and vault account was fully audited by SmartDec, a firm specialized in Smart Contract Security Audit. No single authority has control.
+    </p>
+    </div>
+    <div class="advTempl">
+    <div class="container">
+    <span class="captionDark center">Why choose us?</span>
+    <div class="advWrap">
+    <div class="grid">
+    <div class="item col4">
+    <div class="in">
+    <span class="icon-phone gradTxt"></span>
+    <span class="title">AUTOMATED</span>
+    <p>No financial advisor? No experience trading cryptocurrency? No worries. Our algorithm takes away the guesswork and executes transactions automatically on your behalf.</p>
+    </div>
+    </div>
+    <div class="item col4">
+    <div class="in">
+    <span class="icon-chart gradTxt"></span>
+    <span class="title">SMART TRADING</span>
+    <p>Emotions and stress have a negative impact on trading. Our Automated platform allows you to avoid that and gets the best option by analysing all data storage.</p>
+    </div>
+    </div>
+    <div class="item col4">
+    <div class="in">
+    <span class="icon-team gradTxt"></span>
+    <span class="title">PROTECTION FROM HACKING</span>
+    <p>On a decentralized platform, all user accounts are independent; if one account is hacked, this won’t breach the security.</p>
+    </div>
+    </div>
+    <div class="item col4">
+    <div class="in">
+    <span class="icon-docs gradTxt"></span>
+    <span class="title">LOW COST</span>
+    <p>Robot Trading only use money as cost of operation from profit, Thus RTD is ICO lowest cost of operations.</p>
+    </div>
+    </div>
+    <div class="item col4">
+    <div class="in">
+    <span class="icon-cash gradTxt"></span>
+    <span class="title">NO MORE MIDDLE MEN</span>
+    <p>Immediate funding without third-parties. Smart contracts autonomously perform funding – collect and release payments.</p>
+    </div>
+    </div>
+    <div class="item col4">
+    <div class="in">
+    <span class="icon-pass gradTxt"></span>
+    <span class="title">AUTO TRADING 24/7</span>
+    <p>Our platform is always available to ensure that no market opportunity is lost. Let our platform work for you!</p>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
 
 
 
