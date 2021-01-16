d793bd18a4b896a3af444a5401c7ed6b5982271f
Igboanugwo Collins <munisco12@gmail.com>
2021-01-16 08:40:31 +0100

    Added the icons

--- src/containers/AboutUs.js
+++ src/containers/AboutUs.js
@@ -6,57 +6,259 @@ import Particles from 'react-particles-js'
 function AboutUs() {
     return (
         <>
-        <Particles
-                className='particles'
-                params={{
-                    particles: {
-                        number: {
-                            value: 30,
-                        },
-                    },
-                }}
-            />
-        <div className='about'>
-            <h2 className='heading-2 about__header'>At A GLANCE</h2>
-            <div className='about__content'>
-                <h3 className='heading-3 about__heading'>
-                    Welcome to Coinperfectinvestment
-                </h3>
-                <p className='about__text'>
-                    Coinperfectinvestment.com is one of the largest crypto
-                    investment. We have been servicing our clients since 2019
-                    and we are expanding our market share till this day. Our
-                    exclusive location brings us an opportunity to work with
-                    clients anywhere from the coast of the Norwegian Sea to the
-                    shores of Western Europe.
-                </p>
-                <h3 className='heading-3 about__heading'>Our Vision</h3>
-                <p className='about__text'>
-                    Our vision is to continuously seek for ways in which to
-                    feature worth for our investors and make sure we tend to
-                    never lose sight of our company values and core vision. Our
-                    targeted senior management team is committed to making sure
-                    that our high standards are maintained, our regulation is
-                    strict and our complete support is given passion and energy.
-                </p>
-                <h3 className='heading-3 about__heading'>Our Mission</h3>
-                <p className='about__text'>
-                    Our mission is to deliver to clients wonderful services
-                    through our hard working, innovatives services and full
-                    commitments to clients’ satisfaction. We have a tendency to
-                    apply modern fast technology and use a team of extremely
-                    experienced professionals to bring clients’ monetary dreams
-                    in to real life. Our passion is to continually work hard to
-                    assist clients get the most effective potential value and
-                    cut price.
-                </p>
-            </div>
+        
 
-            <div className='about__cryptoMarket'>
+<style>
+.sbmt {background-color: #008CBA; 
+width: 300px;
+border: none;
+color: white;
+padding: 15px 32px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;}
 
-            <CryptoMarketWatch />
-            </div>
-        </div>
+.sbmt:hover {background-color: #2fa076;
+width: 300px;
+border: none;
+color: white;
+padding: 15px 32px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;}
+</style>
+
+</head>
+<body>
+
+<div style="position: fixed; z-index: 99999; left: 20px; bottom: 20px;">
+	<a href="http://wa.me/+447723536106">
+	<img src="https://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png" width="50px" />
+</a>
+</div>
+
+<div class="bodyWrap">
+<header>
+<div class="topLine">
+<div class="container">
+<div class="table full">
+<div class="logoWrap tCell">
+<div class="logo invisLink"> 
+<img src="img/logos.png" alt="logo" height="70px;" />
+Robot 22 Trade
+<a href="index.php">main</a>
+</div>
+</div>
+<div class="logReg tCell right">
+<a href="login.php" class="btnFillColor1Sm">login</a>
+<a href="register.php" class="btnFillColor2Sm">sign up</a>
+</div>
+
+<div class="socContHead lang tCell right">
+<a href="#" style="background-image: url(img/lang_1.jpg);"></a>
+<a href="#" style="background-image: url(img/lang_2.jpg);"></a>
+</div>
+
+
+
+
+<div class="clock tCell right">
+<span class="time gradTxt" id="time"></span>
+<span class="date">
+<span class="day" id="day"></span>
+<span class="year" id="year"></span>
+</span>
+</div>
+
+
+
+
+</div>
+</div>
+</div>
+
+<script src="../widgets.coingecko.com/coingecko-coin-price-marquee-widget.js"></script>
+<coingecko-coin-price-marquee-widget  coin-ids="bitcoin,ethereum,eos,ripple,litecoin" currency="usd" background-color="#ffffff" locale="en"></coingecko-coin-price-marquee-widget>
+
+<div class="menu">
+<div class="container">
+<center><ul>
+<li><a href="index.php">HOME</a></li>
+<li><a href="about.php">ABOUT US</a></li>
+<li><a href="top-reward.php">TOP REWARD</a></li>
+<li><a href="pricing.php">PRICING</a></li>
+<li><a href="testimonies.php">Testimonies</a></li> 
+<li><a href="contact.php">CONTACT US</a></li>
+<li><a href="faq.php">FAQ</a></li>
+</ul></center>
+</div>
+</div>
+</header>
+
+
+
+
+<div class="pageBanner" style="background-image: url(img/about_page_bg.jpg);">
+<div class="container">
+<span class="heading">About us</span>
+</div>
+</div>
+
+<div class="pageAbout" style="background-image: url(img/grey_bg_3.png)">
+<div class="container">
+<div class="textBlock">
+<div class="info right">
+<div class="license">
+<div class="img invisLink" style="background-image: url(img/qr-code.jpg);"><a href="https://beta.companieshouse.gov.uk/company/12143668" target="_blank">license</a></div>
+ 
+</div>
+<div class="address">
+<div class="iconLeft">
+<span class="icon-location gradTxt"></span>
+<span class="data">71-75 Shelton Street, London, Greater London,
+United Kingdom, WC2H 9JQ</span>
+</div>
+</div>
+<div class="btnsWrap" style="text-align: center;">
+<a href="register.php" class="btnFillColor1Lg" style="margin-right: 20px;">Join</a>
+<a href="https://beta.companieshouse.gov.uk/company/12143668" target="_blank" class="btnFillColor2Lg" style="margin: 20px 0 0;">Check registration</a>
+</div>
+</div>
+<p>
+Robot 22 Trade is the only company dedicated to work on EA, better known as Algo Trading, which atomizes the trading process and brings risk free profit, saves times, money and build better wealth over long period of time.
+</p>
+<p>
+A Robot trading System is used to automatically generate trading signals on the forex trader’s behalf. As such, a manual trader has to glue himself or herself in front of a computer screen and wait for the appropriate time when the criteria for making a decision fits with his or her rules—something which is tiring and complicated.
+</p>
+<p>
+With robot trading, the trading rules or strategies have been ingrained in a computer program, and your trade decisions will be made automatically without your intervention. With a trading robot, you can trade profitably 24 hours a day and increase your profits. After you’ve coded your strategies on a robot, it will perform the buy and sell actions while you spend your time on the beach. One of the main reasons why traders fail is emotional trading. However, a forex robot lowers or eliminates emotions from the trading equation and give you a reliable way of earning money.
+</p>
+
+
+<div class="img left" style="background-image: url(img/about_img.jpg);"></div>
+<div class="cfix"></div>
+<br/><br/>
+<p>
+Using a blockchain is cryptographically secure – the ledger is distributed across all nodes in the system making hacking near impossible. All of the solidity Ethereum smart contract code used for the token sale and vault account was fully audited by SmartDec, a firm specialized in Smart Contract Security Audit. No single authority has control.
+</p>
+</div>
+<div class="advTempl">
+<div class="container">
+<span class="captionDark center">Why choose us?</span>
+<div class="advWrap">
+<div class="grid">
+<div class="item col4">
+<div class="in">
+<span class="icon-phone gradTxt"></span>
+<span class="title">AUTOMATED</span>
+<p>No financial advisor? No experience trading cryptocurrency? No worries. Our algorithm takes away the guesswork and executes transactions automatically on your behalf.</p>
+</div>
+</div>
+<div class="item col4">
+<div class="in">
+<span class="icon-chart gradTxt"></span>
+<span class="title">SMART TRADING</span>
+<p>Emotions and stress have a negative impact on trading. Our Automated platform allows you to avoid that and gets the best option by analysing all data storage.</p>
+</div>
+</div>
+<div class="item col4">
+<div class="in">
+<span class="icon-team gradTxt"></span>
+<span class="title">PROTECTION FROM HACKING</span>
+<p>On a decentralized platform, all user accounts are independent; if one account is hacked, this won’t breach the security.</p>
+</div>
+</div>
+<div class="item col4">
+<div class="in">
+<span class="icon-docs gradTxt"></span>
+<span class="title">LOW COST</span>
+<p>Robot Trading only use money as cost of operation from profit, Thus RTD is ICO lowest cost of operations.</p>
+</div>
+</div>
+<div class="item col4">
+<div class="in">
+<span class="icon-cash gradTxt"></span>
+<span class="title">NO MORE MIDDLE MEN</span>
+<p>Immediate funding without third-parties. Smart contracts autonomously perform funding – collect and release payments.</p>
+</div>
+</div>
+<div class="item col4">
+<div class="in">
+<span class="icon-pass gradTxt"></span>
+<span class="title">AUTO TRADING 24/7</span>
+<p>Our platform is always available to ensure that no market opportunity is lost. Let our platform work for you!</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="payments">
+<div class="container">
+<ul class="table full">
+<li class="tCell bottom left"><img src="img/payment_1.png" alt="payment"></li>
+<li class="tCell middle left"><img src="img/payment_2.png" alt="payment"></li>
+<li class="tCell middle right"><img src="img/payment_4.png" alt="payment"></li>
+<li class="tCell middle right"><img src="img/payment_5.png" alt="payment"></li>
+<li class="tCell middle right"><img src="img/payment_6.png" alt="payment"></li>
+<li class="tCell middle right"><img src="img/payment_8.png" alt="payment"></li>
+</ul>
+</div>
+</div>
+<!----VisualHyip.com Footer---->
+
+
+
+
+
+<footer>
+<div class="container">
+<div class="cfix">
+<div class="footCol">
+
+
+<p class="copyright">2021 © Robot22 Trade</p>
+</div>
+<ul class="footMenu">
+<li class="col">
+<ul>
+<li><a href="index.php">HOME</a></li>
+<li><a href="about.php">ABOUT US</a></li>
+<li><a href="top-reward.php">TOP REWARD</a></li>
+<li><a href="pricing.php">PRICING</a></li>
+</ul>
+</li>
+<li class="col">
+<ul>
+<li><a href="testimonies.php">TESTIMONIES</a></li>
+<li><a href="contact.php">CONTACT US</a></li>
+<li><a href="faq.php">FAQ</a></li> 
+<li><a href="register.php">Sign up</a></li>
+</ul>
+</li>
+</ul>
+<div class="footCol">
+<div class="cont">
+<span class="title">Adress:</span>
+<p>71-75 Shelton Street, London, Greater London,
+United Kingdom, WC2H 9JQ</p>
+</div>
+<div class="cont">
+<span class="title">E-mail:</span>
+<a class="link" href="mailto:admin@robot22trade.com">admin@robot22trade.com</a>
+</div>
+</div>
+</div>
+</div>
+</footer>
+</div>
+
+
         </>
     )
 }
