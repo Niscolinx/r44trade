16f0bcc0db35207b5be49af5732ea5f5b9f0ab07
Igboanugwo Collins <munisco12@gmail.com>
2021-01-16 11:38:01 +0100

    Working on the topreward page

---
+++ src/containers/TopReward.js
@@ -0,0 +1,116 @@
+import React from 'react'
+
+const TopReward =() =>{
+    return (
+        <>
+            <div class="pageBanner" style="background-image: url(img/partners_page_bg.jpg);">
+<div class="container">
+<span class="heading">Top Reward</span>
+</div>
+</div>
+
+<div class="progrDescr">
+<div class="container">
+<div class="textBlock">
+<p>
+This is a list of all our top rewarders, We are very client centric to our customers and
+All of the solidity Ethereum smart contract code used for the token sale and vault account was fully audited by SmartDec, a firm specialized in Smart Contract Security Audit. No single authority has control.
+</p>
+</div>
+</div>
+</div>
+
+<div class="program page gradBack ">
+<div class="container">
+<div class="wrap">
+<div class="row">
+
+ 
+<style type="text/css">
+    .tCell{
+        padding-top: 10px;
+        font-weight: 500;
+        padding-bottom: 10px;
+    }
+</style>
+
+    
+<div class="inner">
+<div class="title">
+<span style="font-size: 30px; font-weight: 600;">TOP REWARD</span>
+</div>
+<ul class="table full in">
+
+
+
+<li class="tRow"><span class="tCell middle name">Ms. Willie Hoppe III</span><span class="tCell middle date"> </span><span class="tCell middle sum">$95,100.00</span><span class="tCell bottom right pay"><img src="img/ps_b/1000.png" alt="pay"></span></li><li class="tRow"><span class="tCell middle name">Ms. Willie Hoppe III</span><span class="tCell middle date"> </span><span class="tCell middle sum">$134,000.00</span><span class="tCell bottom right pay"><img src="img/ps_b/1000.png" alt="pay"></span></li><li class="tRow"><span class="tCell middle name">Elissa Murazik</span><span class="tCell middle date"> </span><span class="tCell middle sum">$87,000.00</span><span class="tCell bottom right pay"><img src="img/ps_b/1000.png" alt="pay"></span></li><li class="tRow"><span class="tCell middle name">Dr. Velma Larson PhD</span><span class="tCell middle date"> </span><span class="tCell middle sum">$262,800.00</span><span class="tCell bottom right pay"><img src="img/ps_b/1000.png" alt="pay"></span></li><li class="tRow"><span class="tCell middle name">Takalani Letsoalo</span><span class="tCell middle date"> </span><span class="tCell middle sum">$76,000.00</span><span class="tCell bottom right pay"><img src="img/ps_b/1000.png" alt="pay"></span></li> 
+
+ 
+</ul>
+
+</div>
+
+ 
+
+</div> 
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="steps">
+<div class="row">
+ 
+
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+<a href="register.php" class="btnFillDarkLg center">Register</a>
+
+</div>
+
+
+<div class="circleBg"><div class="circle"></div></div>
+
+
+</div>
+</div>
+</div>   
+</div>
+</div>
+</div>
+
+
+
+
+     </>
+    )
+}
+
+export default TopReward
