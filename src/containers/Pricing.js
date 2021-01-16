ab8d4838506a9cc12bb569bfbca813ce3e05ca68
Igboanugwo Collins <munisco12@gmail.com>
2021-01-16 12:21:03 +0100

    Working on pricing page

--- src/containers/Pricing.js
+++ src/containers/Pricing.js
@@ -2,6 +2,7 @@ import React from 'react'
 import RulesPageBg from '../images/rules_bg_bg.jpg'
 import GreyBgBig from '../images/grey_bg_big.png'
 import CardBg1 from '../images/card_bg_1.png'
+import CardBg2 from '../images/card_bg_2.png'
 
 function Pricing() {
     return (
