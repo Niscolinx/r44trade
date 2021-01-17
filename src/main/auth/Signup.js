9a6da6d396b6c1155b1c248c9778891b8e8a15b0
Igboanugwo Collins <munisco12@gmail.com>
2021-01-17 14:52:38 +0100

    working on the signup

--- src/main/auth/Signup.js
+++ src/main/auth/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
 import { connect } from 'react-redux'
 import Particles from 'react-particles-js'
 
@@ -947,7 +948,7 @@ $(document).ready(function(){
 </div>
 </div>
 </div>
-</div>
+
 
         </>
     )
