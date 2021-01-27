2bb514127b24afa3ad968e87f3fff251117d88d3
Igboanugwo Collins <munisco12@gmail.com>
2021-01-27 05:25:51 +0100

    Moved files permanently

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+
 import { LastLocationProvider } from 'react-router-last-location'
 
 import App from './containers/App'
 
-
-const store = createStore(
-    combineReducers(rootReducer),
-    composeWithDevTools(applyMiddleware(thunk))
-)
 
 const app = (
     <Provider store={store}>
