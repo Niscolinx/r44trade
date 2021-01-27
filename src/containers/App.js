f09e27570ffbddb150995f58cc1cdcbcdfbc2a53
Igboanugwo Collins <munisco12@gmail.com>
2021-01-27 05:28:01 +0100

    Moved files permanently

---
+++ src/containers/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const App = () => {
+    return (
+        <div>These files have been moved!! </div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import App from './containers/App'
 
 
 const app = (
-    <Provider store={store}>
         <BrowserRouter>
             <LastLocationProvider>
                 <App />
             </LastLocationProvider>
         </BrowserRouter>
-    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'))
